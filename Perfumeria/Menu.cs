4a57883becaec4386a14dc5ef00f606e79e92b51
JulianCaprile <juliancaprile1@gmail.com>
2025-07-07 20:24:25 -0300

    sin Emir

--- Perfumeria/Menu.cs
+++ Perfumeria/Menu.cs
@@ -29,7 +29,7 @@ namespace Perfumeria
 
             // Cuando se cierra, vuelve el logo
             formulario.FormClosed += (s, args) => logo.Visible = true;
-            // Emir
+            
             formulario.Show();
         }
 
